a435f7da8e9922353d9ddba7ca1cd83c65045dc9
adanhm22 <43605705+adanhm22@users.noreply.github.com>
2023-03-17 13:58:11 +0100

    ej hechos

--- ejercicio4.php
+++ ejercicio4.php
@@ -1,3 +1,9 @@
 <?php
+    $people_json = file_get_contents('https://pokeapi.co/api/v2/pokemon?limit=151');
+    $decoded_json = json_decode($people_json, true);
 
+    echo "<h1> LISTA DE POKEMONS</h1>";
+    foreach ($decoded_json["results"] as $key) {
+        echo "<b>".$key['name'] ." ---</b>" . $key['url'] . "<br>";
+    }
 ?>
